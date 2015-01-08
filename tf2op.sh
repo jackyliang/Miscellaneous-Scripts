896b4f09ef048c67abae2400afa112de9d5f6c76
Jacky Liang <bluelegopiece@hotmail.com>
2015-01-08 22:57:19 +0800

    re fix_3: refactorized and removed unnecessary params

--- tf2op.sh
+++ tf2op.sh
@@ -1,13 +1,11 @@
-// Playing around with TF2OP's bump API
+## TF2Output cURL requests
+UHASH="Cookie: uhash=6664edcc2bba5797626d1fd272829dd1"
 
-// Conjurer's 
+# Conjurer's 
+curl 'http://www.tf2outpost.com/api/core' -H "$UHASH" --data 'action=trade.bump&hash=6664edcc2bba5797626d1fd272829dd1&tradeid=20664049'
 
-curl 'http://www.tf2outpost.com/api/core' -H 'Host: www.tf2outpost.com' -H 'Cookie: uhash=6664edcc2bba5797626d1fd272829dd1;' --data 'action=trade.bump&hash=6664edcc2bba5797626d1fd272829dd1&tradeid=20664049'
+# Howl
+curl 'http://www.tf2outpost.com/api/core' -H "$UHASH" --data 'action=trade.bump&hash=6664edcc2bba5797626d1fd272829dd1&tradeid=22315747'
 
-// Howl
-
-curl 'http://www.tf2outpost.com/api/core' -H 'Host: www.tf2outpost.com' -H 'Cookie: uhash=6664edcc2bba5797626d1fd272829dd1;' --data 'action=trade.bump&hash=6664edcc2bba5797626d1fd272829dd1&tradeid=22315747'
-
-// Soldier
-
-curl 'http://www.tf2outpost.com/api/core' -H 'Host: www.tf2outpost.com' -H 'Cookie: uhash=6664edcc2bba5797626d1fd272829dd1;' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' --data 'action=trade.bump&hash=6664edcc2bba5797626d1fd272829dd1&tradeid=24526074'
+# Soldier
+curl 'http://www.tf2outpost.com/api/core' -H "$UHASH" --data 'action=trade.bump&hash=6664edcc2bba5797626d1fd272829dd1&tradeid=24526074'
