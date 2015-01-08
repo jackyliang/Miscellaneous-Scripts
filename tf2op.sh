b8288a1a5a8945dd7b435998362756fc6b84b0d2
Jacky Liang <bluelegopiece@hotmail.com>
2015-01-08 23:06:20 +0800

    re #3: further refactorization

--- tf2op.sh
+++ tf2op.sh
@@ -1,11 +1,13 @@
 ## TF2Output cURL requests
-UHASH="Cookie: uhash=6664edcc2bba5797626d1fd272829dd1"
 
-# Conjurer's 
-curl 'http://www.tf2outpost.com/api/core' -H "$UHASH" --data 'action=trade.bump&hash=6664edcc2bba5797626d1fd272829dd1&tradeid=20664049'
+# TF2OP Cookie
+HASH="6664edcc2bba5797626d1fd272829dd1"
 
-# Howl
-curl 'http://www.tf2outpost.com/api/core' -H "$UHASH" --data 'action=trade.bump&hash=6664edcc2bba5797626d1fd272829dd1&tradeid=22315747'
+# Trade IDs
+CONJURER_COWL="20664049"
+M4A4_HOWL="22315747"
+SOLDIER="24526074"
 
-# Soldier
-curl 'http://www.tf2outpost.com/api/core' -H "$UHASH" --data 'action=trade.bump&hash=6664edcc2bba5797626d1fd272829dd1&tradeid=24526074'
+curl 'http://www.tf2outpost.com/api/core' -H "Cookie: uhash=$HASH" --data "action=trade.bump&hash=$HASH&tradeid=$CONJURER_COWL"
+curl 'http://www.tf2outpost.com/api/core' -H "Cookie: uhash=$HASH" --data "action=trade.bump&hash=$HASH&tradeid=$M4A4_HOWL"
+curl 'http://www.tf2outpost.com/api/core' -H "Cookie: uhash=$HASH" --data "action=trade.bump&hash=$HASH&tradeid=$SOLDIER"
