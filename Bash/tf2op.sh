eaae68a4a098da0beb0aaba0e3209ff762e5c6a2
Jacky Liang <bluelegopiece@hotmail.com>
2015-03-07 23:19:11 +0800

    updated new tf2op.sh script

--- Bash/tf2op.sh
+++ Bash/tf2op.sh
@@ -4,10 +4,7 @@
 HASH="5460856c6b5a29c1573075aa5d44b641"
 
 # Trade IDs
-CONJURER_COWL="20664049"
-M4A4_HOWL="22315747"
-SOLDIER="24526074"
+BUDS="25285054"
 
-curl 'http://www.tf2outpost.com/api/core' -H "Cookie: uhash=$HASH" --data "action=trade.bump&hash=$HASH&tradeid=$CONJURER_COWL"
-curl 'http://www.tf2outpost.com/api/core' -H "Cookie: uhash=$HASH" --data "action=trade.bump&hash=$HASH&tradeid=$M4A4_HOWL"
-curl 'http://www.tf2outpost.com/api/core' -H "Cookie: uhash=$HASH" --data "action=trade.bump&hash=$HASH&tradeid=$SOLDIER"
+curl 'http://www.tf2outpost.com/api/core' -H "Cookie: uhash=$HASH"
+--data "action=trade.bump&hash=$HASH&tradeid=$BUDS"
